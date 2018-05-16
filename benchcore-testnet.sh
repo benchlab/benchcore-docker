7bf9ef52579b0ede6d24fb55939b6df10f921f52
BenchLabs <38194539+benchlab@users.noreply.github.com>
2018-05-15 19:24:22 -0500

    Create benchcore-testnet.sh

---
+++ benchcore-testnet.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+## BENCHCORE INSTALLER BASH SCRIPT 
+## 
+
+
+## BenchCore Application Input Params
+BENCH=/benchcore/${BENCH:-benchcore}
+ID=${ID:-0}
+LOG=${LOG:-benchcore-testnet.log}
+
+##
+## Install BenchCore Application On Linux
+##
+if ! [ -f "${BENCH}" ]; then
+	echo "The BenchCore application $(basename "${BENCH}") was not found. Please add the BenchCore application to the folder inside the '$BENCH' variable within 'benchcore-testnet.sh' . Please use the BENCH environment variable if the name of the BenchCore application is not 'benchcore' E.g.: -e BENCH=customcore"
+	exit 1
+fi
+BENCH_CHECK="$(file "$BENCH" | grep 'ELF 64-bit LSB executable, x86-64')"
+if [ -z "${BENCH_CHECK}" ]; then
+	echo "Server OS needs to be a flavor of Linux (Ubuntu, Debian, CentOS) or ARCH amd64"
+	exit 1
+fi
+
+##
+## Run BenchCore Application With All Available Params
+##
+export BCHOME="/benchcore/node${ID}"
+
+if [ -d "`dirname ${BCHOME}/${LOG}`" ]; then
+  "$BENCH" "$@" | tee "${BCHOME}/${LOG}"
+else
+  "$BENCH" "$@"
+fi
+
+chmod 777 -R /benchcore
