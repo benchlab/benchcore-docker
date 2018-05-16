c2925c6b6b19dd9f4e5242143c51c8781e90d0ca
BenchLabs <38194539+benchlab@users.noreply.github.com>
2018-05-15 19:39:26 -0500

    Update benchcore-testnet.sh

--- benchcore-testnet.sh
+++ benchcore-testnet.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 ## BENCHCORE INSTALLER BASH SCRIPT 
-## 
+## Copyright 2018 Bench Computer, Inc. All rights reserved.
+## Use of this source code is governed by the BENCH License
+## The BENCH license can be found in the LICENSE file in the root directory of this software.
+## This software my utilize other software libraries that are mentioned in benOS License Agreement
+## For the license agreement, go to: https://github.com/benchlab/benOS/blob/master/ATTRIBUTES.md
+## Using this software to issue illegal securities is prohibited.
 
 
 ## BenchCore Application Input Params
